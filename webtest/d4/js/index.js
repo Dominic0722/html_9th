12c60941bc0ac6ff7b394c4592af030df90eec5f
dominic8722@gmail.com <dominic8722@gmail.com>
2024-11-07 22:08:30 +0900

    d4done

--- webtest/d4/js/index.js
+++ webtest/d4/js/index.js
@@ -49,25 +49,46 @@
 //         modal.classList.remove("on")
 //     })
 // })
+// window.addEventListener("load",function(){
+//     let btn_open=this.document.querySelector(".btn_open")
+//     let popup=this.document.querySelector(".popup")
+//     let btn_close=this.document.querySelector(".btn_close")
+
+//     btn_open.addEventListener("click",function(){
+//         popup.classList.add("on")
+//     })
+//     btn_close.addEventListener("click",function(){
+//         popup.classList.add("on")
+//     })
+//     let tabs=this.document.querySelectorAll(".tabs>a")
+//     let tab_contents=this.document.querySelectorAll(".tab_contents>ul")
+//     tabs.forEach(function(tab,index){
+//         tab.addEventListener("click",function(){
+//             tabs[0].classList.remove("on")
+//             tabs[1].classList.remove("on")
+//             tab.classList.add("on")
+//         })
+//     })
+
+// })
 window.addEventListener("load",function(){
     let btn_open=this.document.querySelector(".btn_open")
     let popup=this.document.querySelector(".popup")
     let btn_close=this.document.querySelector(".btn_close")
-
+    let train=this.document.querySelectorAll(".train>li")
+    let count=0
+    this.setInterval(function(){
+        count++
+        if(count>2){count=0}
+        train[0].classList.remove("on")
+        train[1].classList.remove("on")
+        train[2].classList.remove("on")
+        train[count].classList.add("on")
+    },2500)
     btn_open.addEventListener("click",function(){
         popup.classList.add("on")
     })
     btn_close.addEventListener("click",function(){
-        popup.classList.add("on")
+        popup.classList.remove("on")
     })
-    let tabs=this.document.querySelectorAll(".tabs>a")
-    let tab_contents=this.document.querySelectorAll(".tab_contents>ul")
-    tabs.forEach(function(tab,index){
-        tab.addEventListener("click",function(){
-            tabs[0].classList.remove("on")
-            tabs[1].classList.remove("on")
-            tab.classList.add("on")
-        })
-    })
-
 })
