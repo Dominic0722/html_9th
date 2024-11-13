92ae54d5c7a66c5ac9b5230bddedc1ef74cff421
DD <dominic8722@gmail.com>
2024-11-13 18:10:56 +0900

    hades start

---
+++ webtest/c1/js/index2.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $(".btn_open").click(function(){
+        $(".popup").addClass("on")
+    })
+    $(".btn_close").click(function(){
+        $(".popup").removeClass("on")
+    })
+
+    let count=0
+
+    setInterval(function(){
+        count++
+        if(count>2){count=0}
+        $(".train>li").removeClass("on")
+        $(".train>li").eq(count).addClass("on")
+    },2500)
+
+   
+    $(".tabmenu>dt").click(function(){
+        $(".tabmenu>dt").removeClass("on")
+        $(this).addClass("on")
+    })
+})
